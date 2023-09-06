384f886816f5ad4931e4623fc6b01bfd95c14532
Halleriet <floris.halleriet@student.graafschapcollege.nl>
2023-09-06 11:38:13 +0200

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_layout_challenge/pages/page1.dart';
+import 'package:flutter_layout_challenge/view/pages/page1.dart';
 
 void main() {
   runApp(const MainApp());
