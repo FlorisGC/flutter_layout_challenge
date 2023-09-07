f463a919ce122ee9ed6dd3c4dd58fccfbf5e81b0
Halleriet <floris.halleriet@student.graafschapcollege.nl>
2023-09-07 10:45:43 +0200

    problem fixes

--- lib/controller/mytasksbox_addons/tasksbutton.dart
+++ lib/controller/mytasksbox_addons/tasksbutton.dart
@@ -5,19 +5,15 @@ class TasksButton extends StatelessWidget {
   final double iconSize = 50;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: iconSize,
-      height: iconSize,
-      decoration:
-          const BoxDecoration(shape: BoxShape.circle, color: Colors.teal),
-      child: IconButton(
-        onPressed: () {
-          
-        },
-        icon: const Icon(Icons.date_range_rounded),
-        color: Colors.white,
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Container(
+        width: iconSize,
+        height: iconSize,
+        decoration:
+            const BoxDecoration(shape: BoxShape.circle, color: Colors.teal),
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.date_range_rounded),
+          color: Colors.white,
+        ),
+      );
 }
